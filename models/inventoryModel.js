a1ffb806a6eab92819bcf9e10b3c364accec7fda
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-06-20 22:34:19 +1000

    inventory model setup

---
+++ models/inventoryModel.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import userModel from './userModel.js';
+
+const inventorySchema = new mongoose.Schema({
+  inventoryType: {
+    type: String,
+    required: [true, 'inventory type required'],
+    enum: ['in', 'out'],
+  },
+  bloodGroup: {
+    type: String,
+    required: [true, 'blood group type required'],
+    enum: ['O+', 'O-', 'AB+', 'AB-', 'A-', 'B+', 'B-'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'blood quantity is required'],
+  },
+  organisation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: userModel,
+    required: [true, 'organisation is required'],
+  },
+  hospital: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: userModel,
+    required: function () {
+      return this.inventoryType === 'out';
+    },
+  },
+  donor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: userModel,
+    required: function () {
+      return this.inventoryType === 'in';
+    },
+  },
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Inventory', inventorySchema);
