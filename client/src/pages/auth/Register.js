e0c24631ed0c1746867c78494d3c078c3e05b9d9
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-06-24 00:30:33 +1000

    register page

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Box, Grid, Container } from "@mui/material";
+
+import { useStyles } from "../../styles/loginStyles";
+
+import Form from "../../components/shared/Form/Form";
+import RegisterWallpaper from "../../assets/RegisterWallpaper.jpg";
 
 const Register = () => {
+  const classes = useStyles();
   return (
-    <div>Register page</div>
-  )
-}
+    <>
+      <Box>
+        <Grid container>
+          <Grid item lg={7.5} sm={12}>
+            <Box>
+              <img
+                src={RegisterWallpaper}
+                alt="#wallpaper"
+                className={classes.registerImage}
+              />
+            </Box>
+          </Grid>
+          <Grid item lg={4.5} sm={12}>
+            <Container sx={{ width: "80%", padding: "1rem 0" }}>
+              <Box>
+                <Form
+                  submitBtn={"Register"}
+                  formTitle={"Register Page"}
+                  formType={"register"}
+                />
+              </Box>
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
 
-export default Register
+export default Register;
