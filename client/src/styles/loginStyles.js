c893202549fe805dbabf502f563b27ffcb9780e0
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-06-24 00:30:43 +1000

    styles

--- client/src/styles/loginStyles.js
+++ client/src/styles/loginStyles.js
@@ -1,19 +1,35 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
-  image: {
+  registerImage: {
+    height: "110vh",
+    width: "100%",
+  },
+  loginImage: {
     height: "100vh",
     width: "100%",
   },
 
   applyFlex: {
     display: "flex",
-    justifyContent: "center",
-    gap: theme.spacing(3),
+    gap: theme.spacing(2),
   },
 
-  buttonBackground: {
-    padding: "1rem",
-    color: "white",
+  applyFlexCol: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: theme.spacing(2),
+  },
+
+  buttonPadding: {
+    padding: "0.6rem",
+  },
+
+  styleForm: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    gap: theme.spacing(2),
   },
 }));
