2f66d388e9a17409c54df32033d05fa1d7541fc8
Kamal Soni <94995608+Ksoni3@users.noreply.github.com>
2023-06-24 21:45:18 +1000

    wrapped app with toast contianer

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Homepage from './pages/auth/Homepage'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Homepage from "./pages/auth/Homepage";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path= '/login' element= {<Login/>}>
-      </Route>
-      <Route path= '/register' element= {<Register/>}>
-      </Route>
-      <Route path= '/' element= {<Homepage/>}>
-      </Route>
-    </Routes>
-    
-  )
-}
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
